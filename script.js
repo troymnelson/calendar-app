92e8b83a442bdf47faa6a78a67994b92bd071047
unknown <troymnlsn@gmail.com>
2022-07-07 15:19:56 -0500

    JD fixed it

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ var container = document.querySelector('.container');
 var today = moment().format('MMMM Do YYYY, h:mm:ss a');;
 console.log(today);
 currentDayEl.textContent = today;
+var hour = moment().hour();
 
 for (var i = 0; i < 9; i++) {
     var timeBlockEl = document.createElement('div');
     timeBlockEl.className = 'time-block';
     container.appendChild(timeBlockEl);
-    
+    timeBlockEl.classList.add('time-' + i);
     var timeEl = document.createElement('p');
     timeEl.className = 'time';
     if (i === 3) {
@@ -24,11 +25,14 @@ for (var i = 0; i < 9; i++) {
     inputTextEl.className = 'user-input';
     var btnEl = document.createElement('button');
     btnEl.className = 'btn';
+    btnEl.dataset.number = i;
     btnEl.textContent = 'save';
     timeBlockEl.append(timeEl, inputTextEl, btnEl);
 
 
-
+    if (timeBlockEl.classList.contains('time-' + i) && hour === i) {
+        
+    }
 
     
 }
@@ -36,11 +40,13 @@ for (var i = 0; i < 9; i++) {
 var allTimeBlocks = document.querySelectorAll('.time-block');
 console.log(allTimeBlocks);
 
-for (var i = 0; i < allTimeBlocks.length; i++) {
 
-    // You need to use a this property
-    btnEl.addEventListener('click', function() {
-        var userText = inputTextEl.value;
-        console.log(userText);
-    });
-}
+
+$('.btn').click(function () {
+    var userText = $(this).parent().find('.user-input').val();
+    // $(this).parent().find('.user-input').addClass();
+    localStorage.setItem('userText-' + $(this).data('number'), JSON.stringify(userText))
+    // localStorage.getData('userText');
+    console.log(userText);
+        
+})
